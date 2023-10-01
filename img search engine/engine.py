408bf93ea90ab76fb9580186a00ae09b3a104680
Ajendrashukla <115613319+Ajendrashukla@users.noreply.github.com>
2023-10-01 16:14:49 +0530

    Update engine.py
    API key removed 
    Auto spell checker for query
    return query as well

--- img search engine/engine.py
+++ img search engine/engine.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request
 import requests
-import pandas as pd
+from spellchecker import SpellChecker
+
+spell=SpellChecker()
 
 app = Flask(__name__)
 
-client_id = "BQJAUDaE0LOyzqw2U34XRCBJXkkhi1OfpJy6Uh4awCQ"
+client_id = "Your API KEY Here"
 
 @app.route('/', methods=['GET', 'POST'])
 def img_search_engine():
@@ -12,6 +14,7 @@ def img_search_engine():
     result=[]
     if request.method == 'POST':
         query = request.form['query']
+        query=spell.correction(query)
         url = f"https://api.unsplash.com/search/photos?page=all&query={query}&client_id={client_id}"
         response = requests.get(url)
         response = response.json()
@@ -28,10 +31,9 @@ def img_search_engine():
                 l.append(temp["urls"]["raw"])
                 result.append(l)
             result=sorted(result,key=lambda x:x[1],reverse=True)
-            print(pd.DataFrame(result))
 
     
-    return render_template("index.html", data=result)
+    return render_template("index.html", data=result,query=query)
 
 if __name__ == '__main__':
     app.run(debug=False)
