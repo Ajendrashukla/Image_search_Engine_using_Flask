992323ad27d6167c3b13be0cc09cb7692694266e
Ajendrashukla <115613319+Ajendrashukla@users.noreply.github.com>
2023-10-01 19:11:16 +0530

    Update engine.py

--- img search engine/engine.py
+++ img search engine/engine.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request
 import requests
 from spellchecker import SpellChecker
+import pandas as pd
 app = Flask(__name__)
 spell=SpellChecker()
-client_id = "Your Unsplash API key"
+client_id = "Your API KEY"
 
 @app.route('/', methods=['GET', 'POST'])
 def img_search_engine():
@@ -20,22 +21,22 @@ def img_search_engine():
         url = f"https://api.unsplash.com/search/photos?page=all&query={query}&client_id={client_id}"
         response = requests.get(url)
         response = response.json()
-        if int(response['total_pages'])>=10:
-            response= response["results"]
-            for i in range(9):
-                temp= response[i]
-                l=[]
-                l.append(temp["urls"]["small"])
-                l.append(temp["likes"])
-                l.append(temp["alt_description"].capitalize())
-                l.append(temp["urls"]["raw"])
-                result.append(l)
-            result=sorted(result,key=lambda x:x[1],reverse=True)
+        response= response["results"]
+        for i in range(9):
+            temp= response[i]
+            l=[]
+            l.append(temp["urls"]["small"])
+            l.append(temp["likes"])
+            l.append(temp["alt_description"].capitalize())
+            l.append(temp["urls"]["raw"])
+            result.append(l)
+        result=sorted(result,key=lambda x:x[1],reverse=True)
+            
     else:
         query='Please Enter a KeyWOrd to Search Images'
-
+    print(pd.DataFrame(result))
     
     return render_template("index.html", data=result,query=query)
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
